9ea0e8c2db0ae89da78076dcbe2756d6c4e741e5
gjenjoying <revival.wgj@gmail.com>
2022-06-15 16:19:50 +0800

    更新脚本

--- remove_tencent_services.sh
+++ remove_tencent_services.sh
@@ -1,15 +1,6 @@
 #!/bin/bash 
 
 # 参考：https://blog.kdyzm.cn/post/20
-# 先用脚本删除
-/usr/local/qcloud/stargate/admin/uninstall.sh
-/usr/local/qcloud/YunJing/uninst.sh
-/usr/local/qcloud/monitor/barad/admin/uninstall.sh
-
-# 删除 tat_agent
-systemctl stop tat_agent
-systemctl disable tat_agent
-rm -f /etc/systemd/system/tat_agent.service
 
 # 删除 secu-tcs-agent
 process=(sap100 secu-tcs-agent sgagent64 barad_agent agent agentPlugInD pvdriver ) 
@@ -20,6 +11,16 @@ do
 kill -9 $A 
 done 
 done 
+
+# 用脚本删除
+/usr/local/qcloud/stargate/admin/uninstall.sh
+/usr/local/qcloud/YunJing/uninst.sh
+/usr/local/qcloud/monitor/barad/admin/uninstall.sh
+
+# 删除 tat_agent
+systemctl stop tat_agent
+systemctl disable tat_agent
+rm -f /etc/systemd/system/tat_agent.service
 
 ps -A | grep agent
 echo "ps -A | grep agent 执行后，如无任何显示，则删光了；如有，看看是否是自己的 "
